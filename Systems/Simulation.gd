07010ffcb83d36d76bcbd7f3c070c953dd9a58e8
Stian Sørby <ssouthcity@gmail.com>
2021-07-29 03:23:59 +0200

    Completed step: wires

--- Systems/Simulation.gd
+++ Systems/Simulation.gd
@@ -8,11 +8,12 @@ var _tracker := EntityTracker.new()
 
 onready var _ground := $GameWorld/GroundTiles
 onready var _entity_placer := $GameWorld/YSort/EntityPlacer
+onready var _flat_entities := $GameWorld/FlatEntities
 onready var _player := $GameWorld/YSort/Player
 
 
 func _ready() -> void:
-	_entity_placer.setup(_tracker, _ground, _player)
+	_entity_placer.setup(_tracker, _ground, _flat_entities, _player)
 	
 	var barriers: Array = _ground.get_used_cells_by_id(BARRIER_ID)
 	
